4e7b20098afcc8e1403998daa174d9b277cf3eeb
ajarek <61388692+ajarek@users.noreply.github.com>
2021-01-02 12:53:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -25,9 +25,9 @@ function odliczanie(){
 let dzis=new Date()
  let nowyRok=new Date("December 31, 2021 23:59:59");
  let godziny =1000*60*60;
- wynik.innerHTML='Nowy ROK za: ' +*((nowyRok.getTime()-dzis.getTime()))/godziny).toFixed(2) +' h';
-if((nowyRok.getTime()-dzis.getTime())==0){
-alert('Szczęśliwego Nowego 2021 Roku')
+ wynik.innerHTML='Nowy ROK za: ' +((nowyRok.getTime()-dzis.getTime()))/godziny).toFixed(2) +' h';
+if((nowyRok.getTime()-dzis.getTime()/godziny)<0){
+wynik.innerHTML='Szczęśliwego Nowego 2022 Roku';
 }
 }
 setInterval(odliczanie, 1000)
